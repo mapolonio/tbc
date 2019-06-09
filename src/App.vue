8b0e0a34ec3056718644844a31b1acab2b3938ed
Matías Apolonio <mapolonio@acid.cl>
2019-06-09 18:15:21 -0400

    WIP

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,20 @@
 <template>
   <div id="app">
     <h1>Hello World! "{{ message }}"</h1>
+    <h1>Welcome to TBC Finder</h1>
+    <form method="GET">
+      <input type="text" placeholder="Shirt" name="search">
+      <button type="submit">Search</button>
+    </form>
+    <form method="POST">
+      <p>Upload your file to create the search index:</p>
+      <p>
+        <input type="file">
+      </p>
+      <p>
+        <button type="submit">Upload file</button>
+      </p>
+    </form>
   </div>
 </template>
 
