d18ae9662abbd46880fa544617bfc1d3c5c81dd7
Matías Apolonio <mapolonio@acid.cl>
2019-06-09 23:14:46 -0400

    Updated eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,19 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true
-    },
-    "extends": [
-        "eslint:recommended",
-        "plugin:vue/essential"
-    ],
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
-    },
-    "parserOptions": {
-        "ecmaVersion": 2018,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "vue"
-    ],
-    "rules": {
-    }
+  env: {
+    node: true,
+    es6: true
+  },
+  extends: ["eslint:recommended", "plugin:vue/essential"],
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly"
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: "module"
+  },
+  plugins: ["vue"],
+  rules: {
+    quotes: [2, "single", "avoid-escape"]
+  }
 };
